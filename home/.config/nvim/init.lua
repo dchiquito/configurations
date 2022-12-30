36b0cbf2f05cbc1dd5aa4f07470206334139693e
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-12-29 20:58:20 -0500

    Fix nvim comment keybind

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -99,10 +99,10 @@ vim.keymap.set('', '<leader>l', '<cmd>HopWordCurrentLine<cr>', {desc = 'Hop to a
 
 -- comment/uncomment
 -- linewise commenting
-vim.keymap.set({'n', 'i', 'c'}, '<C-_>', function()
+vim.keymap.set({'n', 'i', 'c'}, '<C-/>', function()
   require('Comment.api').toggle.linewise.current()
 end, {desc = 'Toggle comments'})
-vim.keymap.set('v', '<C-_>', function()
+vim.keymap.set('v', '<C-/>', function()
   local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
   vim.api.nvim_feedkeys(esc, 'nx', false)
   require('Comment.api').toggle.linewise(vim.fn.visualmode())
