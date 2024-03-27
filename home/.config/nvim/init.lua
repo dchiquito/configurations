9cf5ed5a560f79c087dd0b890dedd3e4b754ae9f
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-03-26 22:04:21 -0400

    Add neovide scaling hotkeys

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -54,6 +54,17 @@ if vim.fn.exists('g:neovide') then
   vim.opt.titlestring = vim.env.PWD
 end
 
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-=>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1/1.25)
+end)
+
 
 --------------------------------
 -- Keyboard shortcuts/hotkeys --
